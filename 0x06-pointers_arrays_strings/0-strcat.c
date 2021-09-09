c82b0db37e3fd04240353722bc1401fe10d6e467
kosiokpala69 <kosiokpala69@gmail.com>
2021-09-10 00:56:35 +0100

    let it work

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,21 +11,29 @@
 
   {
 
-	    int i, j;
-  	i = j = 0;
+	    int	i = 0
+		int j = 0;
+	    
 	    while (*(dest + i))
 
 		    i++;
-	    while ((*(dest + i) = *(src + j)))
 
-	    {
+          while (*(src + j))
+
+	  {
+
+	    *(dest+i) = *(src + j);
+
+	    
 			i++;
 			
 			j++;
 
 	   }
 
+	  *(dest+i) = '\0';
+
 	    return (dest);
 
-  }  
+  }
 
